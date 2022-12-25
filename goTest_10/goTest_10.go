1dc9ae27d8d72e893baadc3e927bf5bd4a2ca68a
yang20002020 <2628636987@qq.com>
2022-12-25 20:13:31 +0800

    字符串字符个数统计

---
+++ goTest_10/goTest_10.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+/*
+描述
+编写一个函数，计算字符串中含有的不同字符的个数。字符在 ASCII 码范围内( 0~127 ，包括 0 和 127 )，换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
+例如，对于字符串 abaca 而言，有 a、b、c 三种不同的字符，因此输出 3 。
+
+数据范围： 1 <= n <= 500
+输入描述：
+输入一行没有空格的字符串。
+
+输出描述：
+输出 输入字符串 中范围在(0~127，包括0和127)字符的种数。
+
+示例1
+输入：
+abc
+
+输出：
+3
+*/
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inPut := scanner.Text()
+	temp := make(map[byte]int, 1)
+	strArray := []byte(inPut)
+	for i := range strArray {
+		if temp[strArray[i]] > 0 {
+			continue
+		} else {
+			temp[strArray[i]]++
+		}
+	}
+	fmt.Println(len(temp))
+
+}
