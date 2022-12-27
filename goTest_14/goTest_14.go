3de1fdbc677ec2c2a32420488c8b947329238fa3
yang20002020 <2628636987@qq.com>
2022-12-27 18:50:03 +0800

    字符串排序

---
+++ goTest_14/goTest_14.go
@@ -0,0 +1,60 @@
+/*
+
+描述
+给定 n 个字符串，请对 n 个字符串按照字典序排列。
+
+数据范围： 1 <= n <=1000 ，字符串长度满足 1 <=len <= 100
+输入描述：
+输入第一行为一个正整数n(1≤n≤1000),下面n行为n个字符串(字符串长度≤100),字符串中只含有大小写字母。
+输出描述：
+数据输出n行，输出结果为按照字典序排列的字符串。
+示例1
+输入：
+9
+cap
+to
+cat
+card
+two
+too
+up
+boat
+boot
+复制
+输出：
+boat
+boot
+cap
+card
+cat
+to
+too
+two
+up
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	n := 0
+
+	fmt.Scan(&n)
+	str := make([]string, 0)
+	for i := 0; i < n; i++ {
+
+		s := ""
+		fmt.Scan(&s)
+		str = append(str, s)
+	}
+
+	sort.Strings(str)
+
+	for _, v := range str {
+		fmt.Println(v)
+	}
+}
